b7472844b23d73e41a831172aeca6b0c503b914e
bimaadam <rinbim.dev@gmail.com>
2025-03-10 15:18:19 +0700

    Menambahkan fitur Pendapatan Klinik

---
+++ src/app/(dashboard)/PendapatanKlinik/page.tsx
@@ -0,0 +1,6 @@
+import Typography from '@mui/material/Typography'
+export default function PendapatanPage() {
+    return (
+        <Typography variant="h4" color="initial">Pendapatan Klinik</Typography>
+    )
+}
