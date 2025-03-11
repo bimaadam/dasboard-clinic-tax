6d4032eb22866f3f0205ef31d522d0e14eaa2203
bimaadam <rinbim.dev@gmail.com>
2025-03-11 11:13:41 +0700

    Menambahkan menu dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 
-const user = "Admin Klinik"
+const user = "Admin Klinik Setia Medika"
 
 export default function Dashboard() {
   return (
