c21c08a007e3067277dddcc88607e05a7eed07a0
bimaadam <rinbim.dev@gmail.com>
2025-03-12 13:42:00 +0700

    Fungsi Login dan Logout sudah Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Dashboard Laporan Pajak",
-  description: "Laporan Pajak admin dashboard by Bima Adam rin",
+  description: "Laporan Pajak dashboard by Bima Adam rin",
 };
 
 export default function RootLayout({
