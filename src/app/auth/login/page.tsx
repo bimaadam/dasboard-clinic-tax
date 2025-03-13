1ac0dfe1eec48a8c498a8be279d07d1399fb7af3
bimaadam <rinbim.dev@gmail.com>
2025-03-13 12:29:41 +0700

    add page 404

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,55 +1,71 @@
-'use client'
+"use client";
 
-import * as React from 'react';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { SignInPage, type AuthProvider } from '@toolpad/core/SignInPage';
-import { useTheme } from '@mui/material/styles';
+import * as React from "react";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { SignInPage, type AuthProvider } from "@toolpad/core/SignInPage";
+import { createTheme, useTheme } from "@mui/material/styles";
 
+const Theme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: "data-toolpad-color-scheme",
+  },
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+const BRANDING = {
+  logo: <img src="/logo.svg" alt="Logo" style={{ height: 120, width: 250 }} />,
+  title: "Dashboard",
+};
 // preview-start
-const providers = [{ id: 'credentials', name: 'Email and Password' }];
+const providers = [{ id: "credentials", name: "Email and Password" }];
 // preview-end
 
 const signIn: (provider: AuthProvider, formData: FormData) => void = async (
-    provider,
-    formData,
+  provider,
+  formData
 ) => {
-    const email = formData.get("email");
-    const password = formData.get("password");
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    const res = await fetch("/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-    });
+  const res = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
 
-    const data = await res.json(); // Baca response dari server
+  const data = await res.json(); // Baca response dari server
 
-    if (res.ok) {
-        console.log("Login sukses:", data);
-        window.location.href = "/"; // Redirect ke dashboard
-    } else {
-        console.error("Login gagal:", data);
-        alert(data.message || "Cek email dan password");
-    }
+  if (res.ok) {
+    console.log("Login sukses:", data);
+    window.location.href = "/"; // Redirect ke dashboard
+  } else {
+    console.error("Login gagal:", data);
+    alert(data.message || "Cek email dan password");
+  }
 };
 
-
-
 export default function CredentialsSignInPage() {
-    const theme = useTheme();
-    return (
-        // preview-start
-        <AppProvider theme={theme}>
-            <SignInPage
-                signIn={signIn}
-                providers={providers}
-                slotProps={{
-                    emailField: { autoFocus: false },
-                    form: { method: "POST", noValidate: true }, // Paksa pakai POST
-                }}
-            />
-
-        </AppProvider>
-        // preview-end
-    );
+  return (
+    // preview-start
+    <AppProvider branding={BRANDING} theme={Theme}>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        slotProps={{
+          emailField: { autoFocus: false },
+          form: { method: "POST", noValidate: true }, // Paksa pakai POST
+        }}
+      />
+    </AppProvider>
+    // preview-end
+  );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Box, Button, Typography, Container } from "@mui/material";
+import { Box, Button, Typography, Container, createTheme } from "@mui/material";
 import Image from "next/image";
+import { AppProvider } from "@toolpad/core/AppProvider";
 
 export default function NotFound() {
   const router = useRouter();
@@ -15,32 +16,50 @@ export default function NotFound() {
     return () => clearTimeout(timer);
   }, [router]);
 
+  const tema = createTheme({
+    cssVariables: {
+      colorSchemeSelector: "data-toolpad-color-scheme",
+    },
+    colorSchemes: { light: true, dark: true },
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 600,
+        lg: 1200,
+        xl: 1536,
+      },
+    },
+  });
+
   return (
-    <Container maxWidth="md">
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        height="100vh"
-        textAlign="center"
-      >
-        <Image src="/404.svg" alt="404 Not Found" width={300} height={300} />
-        <Typography variant="h3" fontWeight="bold" gutterBottom>
-          Oops! Halaman Tidak Ditemukan
-        </Typography>
-        <Typography variant="body1" color="textSecondary">
-          Mohon Maaf Halaman Yang Anda Cari Tidak Ada
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ mt: 3 }}
-          onClick={() => router.push("/")}
+    <AppProvider>
+      <Container theme={tema} maxWidth="md">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          height="100vh"
+          textAlign="center"
         >
-          Kembali ke Dashboard
-        </Button>
-      </Box>
-    </Container>
+          <Image src="/404.svg" alt="404 Not Found" width={300} height={300} />
+          <Typography variant="h3" fontWeight="bold" gutterBottom>
+            Oops! Halaman Tidak Ditemukan
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            Mohon Maaf Halaman Yang Anda Cari Tidak Ada
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3 }}
+            onClick={() => router.push("/")}
+          >
+            Kembali ke Dashboard
+          </Button>
+        </Box>
+      </Container>
+    </AppProvider>
   );
 }
