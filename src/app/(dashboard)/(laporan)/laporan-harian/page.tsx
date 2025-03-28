fd469738686579820a01b436b3404c6d01823780
bimaadam <rinbim.dev@gmail.com>
2025-03-28 23:27:38 +0700

    fix and deploy to vps server1

--- src/app/(dashboard)/(laporan)/laporan-harian/page.tsx
+++ src/app/(dashboard)/(laporan)/laporan-harian/page.tsx
@@ -1,7 +1,5 @@
+'use client'
 import CrudLaporanHarian from "@/components/TabelPerson";
-import CrudList from "@/components/TabelPerson";
-import Typography from "@mui/material/Typography";
-
 export default function LaporanHarian() {
   return (
     <div className="p-0 w-full mb-10 mx-auto justify-between">
